bf61df02dbce8595069c32b1cd704e3dbab32789
maxbabka <maxbabka@vt.edu>
2021-01-25 15:19:54 -0500

    testing new changes

--- McComasRegistration.py
+++ McComasRegistration.py
@@ -1,19 +1,22 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-
-USERNAME = 'jonathana'
-PASSWORD =
+from datetime import date
 DRIVER_PATH = './chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://connect.recsports.vt.edu/Account/Login')
+driver.get('https://connect.recsports.vt.edu/booking/d71f0446-5a7b-4dee-9549-9f6f3add4812')
 
-try:
-    pidButton = driver.find_element_by_xpath("//button[@title='VT PID']").click() #Click the login button
-    vtUsername = driver.find_element_by_xpath("//input[@id='username']").send_keys(USERNAME)
-    vtPassword = driver.find_element_by_xpath("//input[@id='password']").send_keys(PASSWORD)
-    vtLogin = driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
-except NoSuchElementException:
-    print('Already logged in!')
+today = date.today()
+# try:
+#     pidButton = driver.find_element_by_xpath("//button[@title='VT PID']").click() #Click the login button
+#     vtUsername = driver.find_element_by_xpath("//input[@id='username']").send_keys(USERNAME)
+#     vtPassword = driver.find_element_by_xpath("//input[@id='password']").send_keys(PASSWORD)
+#     vtLogin = driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
+# except NoSuchElementException:
+#     print('Already logged in!')
+
+dates = driver.find_element_by_xpath("//h3[@class='text-primary']")
+print(dates)
+
 
 
 
