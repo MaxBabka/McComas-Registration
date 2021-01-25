775f6b21404051701902a00909776b525b2c0853
maxbabka <maxbabka@vt.edu>
2021-01-25 14:25:11 -0500

    needs valid xpath expression

--- McComasRegistration.py
+++ McComasRegistration.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 
 DRIVER_PATH = './chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://google.com')
+driver.get('https://connect.recsports.vt.edu/Account/Login')
 
-
+pidButton = driver.find_element_by_xpath("//button[@title='VT PID'").click()
