824a905b97a6998e6a15e03873eea3bd46aa6276
maxbabka <maxbabka@vt.edu>
2021-01-25 14:01:03 -0500

    added some lines

--- McComasRegistration.py
+++ McComasRegistration.py
@@ -1,2 +1,7 @@
-print("Hello world!")
-#HELP
+from selenium import webdriver
+
+DRIVER_PATH = './chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://google.com')
+
+
