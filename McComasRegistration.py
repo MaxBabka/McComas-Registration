7757c9cd9e4052209090919d7631eff5318b62d8
maxbabka <maxbabka@vt.edu>
2021-02-11 19:05:36 -0500

    Not working

--- McComasRegistration.py
+++ McComasRegistration.py
@@ -15,21 +15,30 @@ DRIVER_PATH = './chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 driver.get('https://connect.recsports.vt.edu/booking/d71f0446-5a7b-4dee-9549-9f6f3add4812')
 
+login = open(r'Login.txt')
 today = date.today()
 try:
     # login user
     pidButton = driver.find_element_by_xpath("//button[@title='VT PID']").click()  # Click the login button
-    USERNAME = input('Username please')
+    print("CLicked the login button")
+    USERNAME = login.readline()
+    PASSWORD = login.readline()
+    DUO_CODE = login.readline()
+    print(USERNAME)
+    print("About to sleep!")
+    time.sleep(3)
+    print("Finished sleeping!")
     vtUsername = driver.find_element_by_xpath("//input[@id='username']").send_keys(USERNAME)
-    PASSWORD = input('Password please')
+    print("About to sleep!")
+    time.sleep(3)
+    print("Finished sleeping!")
     vtPassword = driver.find_element_by_xpath("//input[@id='password']").send_keys(PASSWORD)
-    vtLogin = driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
+    # vtLogin = driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
 
     # Code here is after you have logged in and are trying to authenticate using Duo Mobile
     driver.switch_to.frame("duo_iframe")
     try:
         time.sleep(4)
-        driver.implicitly_wait(3)
         cancelPush = driver.find_element_by_xpath("//button[@class='btn-cancel']").click()
     except NoSuchElementException:
         print("No cancel button was found!")
@@ -37,24 +46,21 @@ try:
     # These two try catch blocks should try to press the enter a passcode button
     try:
         time.sleep(3)
-        driver.implicitly_wait(3)
         driver.find_element_by_xpath("//button[@class='positive auth-button' and contains(.,'Enter a Passcode ')]") \
             .click()
     except NoSuchElementException:
         print("positive auth not found!")
     try:
-        driver.implicitly_wait(3)
         driver.find_element_by_xpath("//button[@class='auth-button positive' and contains(.,'Enter a Passcode ')]") \
             .click()
     except NoSuchElementException:
         print("auth positive not found!")
-    CODE = input('Input Duo Code please!')
-    duoCode = driver.find_element_by_xpath("//input[@type='text']").send_keys(CODE)
+
+    duoCode = driver.find_element_by_xpath("//input[@type='text']").send_keys(DUO_CODE)
     driver.find_element_by_xpath("//button[@type='submit' and contains(.,'Log In')]").click()
 
 except NoSuchElementException:
     print('Already logged in!')
-print("MADE IT TO HERE")
 
 # At this point you are inside of the McComas's website
 driver.switch_to.default_content
